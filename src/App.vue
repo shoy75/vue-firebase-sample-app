9ead2b32a40600542612f66b54050b33e8216377
shoy75 <yamashiohs@gmail.com>
2020-04-05 00:54:13 +0900

    Get user data from firebase

--- src/App.vue
+++ src/App.vue
@@ -36,6 +36,7 @@ export default {
     firebase.auth().onAuthStateChanged(user => {
       if (user) {
         this.setLoginUser(user)
+        this.fetchAddresses()
         if (this.$router.currentRoute.name === 'home') {
           this.$router.push({ name: 'addresses' }, () => {})
         }
@@ -51,7 +52,7 @@ export default {
     }
   },
   methods: {
-    ...mapActions(['toggleSideMenu', 'setLoginUser', 'logout', 'deleteLoginUser'])
+    ...mapActions(['toggleSideMenu', 'setLoginUser', 'logout', 'deleteLoginUser', 'fetchAddresses'])
   }
 };
 </script>
